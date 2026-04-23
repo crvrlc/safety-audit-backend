e72a6600d3a96b5deedd0948d0508a936207c404
crvrlc <llcervera@up.edu.ph>
2026-04-23 10:33:33 +0800

    fix cors for production

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const managerRoutes = require('./routes/managerRoutes');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use(session({
   secret: process.env.JWT_SECRET,
